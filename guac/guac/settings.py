d63bd8a9f2d9ea857b0d751bf0d61b10984a696d
tomrod10 <tomrod243@gmail.com>
2024-04-19 20:49:18 -0700

    more setup

--- guac/guac/settings.py
+++ guac/guac/settings.py
@@ -75,8 +75,9 @@ WSGI_APPLICATION = "guac.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "guac",
+        "PORT": "8888"
     }
 }
 
